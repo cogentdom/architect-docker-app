7b0eaed7c932b55b18826abf0d986627a48da63d
Dominik <cogentdom@gmail.com>
2021-03-24 09:13:33 -0600

    began implementing docker

--- main.py
+++ main.py
@@ -1,71 +1,80 @@
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 import pandas as pd
+import matplotlib.style as style
+import seaborn as sns
+
+def main():
+    style.use('seaborn-darkgrid')
+    sns.set_context('notebook')
+    sns.set_palette('husl')
+
+    START = "2015-01-01"
+    TODAY = date.today().strftime("%Y-%m-%d")
+
+    st.title("Stock Prediction App")
+
+    stocks = ("AAPL", "GOOG", "MSFT", "TSLA")
+    selected_stock = st.selectbox("Select dataset for prediction", stocks)
+
+    n_years = st.slider("Years of prediction:", 1, 3)
+    period = n_years * 365
 
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-
-st.title("Stock Prediction APP")
-
-stocks = ("AAPL", "GOOG", "MSFT", "TSLA")
-selected_stock = st.selectbox("Select dataset for prediction", stocks)
-
-n_years = st.slider("Years of prediction:", 1, 4)
-period = n_years * 365
+    @st.cache
+    def load_data(ticker):
+        df = yf.download(ticker, START, TODAY)
+        df.reset_index(inplace=True)
+        df['Date'] = pd.to_datetime(df['Date']).dt.date
+        return df
 
 
-@st.cache
-def load_data(ticker):
-    df = yf.download(ticker, START, TODAY)
-    df.reset_index(inplace=True)
-    df['Date'] = pd.to_datetime(df['Date']).dt.date
-    return df
+    data_load_state = st.text("Load data...")
+    data = load_data(selected_stock)
+    data_load_state.text("Loading data...done!")
+
+    st.subheader('Raw data')
+    st.write(data.tail())
 
 
-data_load_state = st.text("Load data...")
-data = load_data(selected_stock)
-data_load_state.text("Loading data...done!")
-
-st.subheader('Raw data')
-st.write(data.tail())
-
-
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+    def plot_raw_data():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+        fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
 
 
-plot_raw_data()
+    plot_raw_data()
 
-# Forecasting
-df_train = data[['Date', 'Close']]
-df_train = df_train.rename(columns={'Date': 'ds', 'Close': 'y'})
+    # Forecasting
+    df_train = data[['Date', 'Close']]
+    df_train = df_train.rename(columns={'Date': 'ds', 'Close': 'y'})
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+    m = Prophet()
+    m.fit(df_train)
+    future = m.make_future_dataframe(periods=period)
+    forecast = m.predict(future)
 
-forecast_copy = forecast.copy()
-forecast_copy['ds'] = pd.to_datetime(forecast_copy['ds']).dt.date
+    forecast_copy = forecast.copy()
+    forecast_copy['ds'] = pd.to_datetime(forecast_copy['ds']).dt.date
 
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast_copy.tail())
+    # Show and plot forecast
+    st.subheader('Forecast data')
+    st.write(forecast_copy.tail())
 
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+    st.write(f'Forecast plot for {n_years} years')
+    fig1 = plot_plotly(m, forecast)
+    st.plotly_chart(fig1)
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+    st.write("Forecast components")
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
+
+
+if __name__ == '__main__':
+    main()
