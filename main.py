29edbe6823164343c88ad4016c3a4b2829326c4e
Dominik <cogentdom@gmail.com>
2021-03-24 08:01:27 -0600

    removed warning tags

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ selected_stock = st.selectbox("Select dataset for prediction", stocks)
 n_years = st.slider("Years of prediction:", 1, 4)
 period = n_years * 365
 
+
 @st.cache
 def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    data['Date'] = pd.to_datetime(data['Date']).dt.date
-    return data
+    df = yf.download(ticker, START, TODAY)
+    df.reset_index(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    return df
+
 
 data_load_state = st.text("Load data...")
 data = load_data(selected_stock)
@@ -33,12 +35,14 @@ data_load_state.text("Loading data...done!")
 st.subheader('Raw data')
 st.write(data.tail())
 
+
 def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
     fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
+
 
 plot_raw_data()
 
@@ -54,7 +58,7 @@ forecast = m.predict(future)
 forecast_copy = forecast.copy()
 forecast_copy['ds'] = pd.to_datetime(forecast_copy['ds']).dt.date
 
-
+# Show and plot forecast
 st.subheader('Forecast data')
 st.write(forecast_copy.tail())
 
